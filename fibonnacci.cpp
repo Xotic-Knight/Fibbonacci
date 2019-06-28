2e468fd7f354b5d39a3e86a3060ea15c4c03d1f5
NNboru <52328161+NNboru@users.noreply.github.com>
2019-06-28 22:36:38 +0530

    Update fibonnacci.cpp

--- fibonnacci.cpp
+++ fibonnacci.cpp
@@ -2,7 +2,7 @@
 main()
 {
 	int first=0,second=1,third,i,n;
-	printf("Enter the number of terms ");
+	printf("Enter the number of terms :");
 	scanf("%d",&n);
 	printf("The fibonnacci series is  %d %d ",first,second);
 	for(i=0;i<n-2;i++)
