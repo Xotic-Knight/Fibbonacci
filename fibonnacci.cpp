293e4c47854515bdfa5ee6f25c53fcb289f63543
Xotic-Knight <52328139+Xotic-Knight@users.noreply.github.com>
2019-06-28 22:15:04 +0530

    Add files via upload

---
+++ fibonnacci.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+main()
+{
+	int first=0,second=1,third,i,n;
+	printf("Enter the number of terms ");
+	scanf("%d",&n);
+	printf("The fibonnacci series is  %d %d ",first,second);
+	for(i=0;i<n-2;i++)
+	{
+		third=first+second;
+		first=second;
+		second=third;
+		printf("%d ",third);
+	}
+}
